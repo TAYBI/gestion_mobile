a0d69f6724e414e3722a7d32c00b30700eda06fd
Bilal <taybib4@gmail.com>
2023-05-25 18:57:48 +0100

    make Client details screen

--- lib/ClientDetailsScreen.dart
+++ lib/ClientDetailsScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'edit_client_screen.dart';
 
 class ClientDetailsScreen extends StatelessWidget {
   final dynamic user;
@@ -8,13 +9,52 @@ class ClientDetailsScreen extends StatelessWidget {
   Widget _buildListItem(IconData icon, String title, String subtitle) {
     return ListTile(
       leading: Icon(icon),
-      title: Text(title),
+      title: Text(
+        title,
+        style: TextStyle(fontSize: 14),
+      ),
       subtitle: Text(subtitle),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    final name =
+        '${user['name']['first']?.toString() ?? ''} ${user['name']['last']?.toString() ?? ''}';
+    final address = user['location']['street']['name']?.toString() ?? '';
+    final city = user['location']['city']?.toString() ?? '';
+    final mobile = user['phone']?.toString() ?? '';
+    final fax = user['cell']?.toString() ?? '';
+    final email = user['email']?.toString() ?? '';
+    final crn = user['id']['value']?.toString() ?? '';
+    final taxId1 = user['id']['name']?.toString() ?? '';
+    final taxId2 = user['login']['sha256']?.toString() ?? '';
+    final startingBalance = user['registered']['date']?.toString() ?? '';
+    final turnover = user['dob']['date']?.toString() ?? '';
+    final payment = user['registered']['age']?.toString() ?? '';
+    final credit = user['login']['password']?.toString() ?? '';
+    final family = user['dob']['age']?.toString() ?? '';
+    final tariff = user['nat']?.toString() ?? '';
+
+    List<Widget> data = [
+      _buildListItem(Icons.person, 'Name', name),
+      _buildListItem(Icons.location_on, 'Address', address),
+      _buildListItem(Icons.location_city, 'City', city),
+      _buildListItem(Icons.phone, 'Telephone', '(123) 456-7890'),
+      _buildListItem(Icons.phone_android, 'Mobile', mobile),
+      _buildListItem(Icons.print, 'Fax', fax),
+      _buildListItem(Icons.email, 'Email', email),
+      _buildListItem(Icons.format_list_numbered, 'CRN', crn),
+      _buildListItem(Icons.credit_card, 'Tax ID', taxId1),
+      _buildListItem(Icons.credit_card_outlined, 'Tax ID2', taxId2),
+      _buildListItem(
+          Icons.monetization_on, 'Starting Balance', startingBalance),
+      _buildListItem(Icons.trending_up, 'Turnover', turnover),
+      _buildListItem(Icons.payment, 'Payment', payment),
+      _buildListItem(Icons.credit_card, 'Credit', credit),
+      _buildListItem(Icons.family_restroom, 'Family', family),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Client'),
@@ -22,37 +62,22 @@ class ClientDetailsScreen extends StatelessWidget {
           IconButton(
             icon: Icon(Icons.edit),
             onPressed: () {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(
-              //     builder: (context) => EditClient(),
-              //   ),
-              // );
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => EditClient(),
+                ),
+              );
             },
           ),
         ],
         centerTitle: true,
       ),
-      body: ListView(
-        children: [
-          _buildListItem(Icons.business, 'Company Name', 'Acme Inc.'),
-          _buildListItem(Icons.person, 'Name', 'Name 1'),
-          _buildListItem(Icons.location_on, 'Address', '123 Main St'),
-          _buildListItem(Icons.location_city, 'City', 'Anytown'),
-          _buildListItem(Icons.phone, 'Telephone', '(123) 456-7890'),
-          _buildListItem(Icons.phone_android, 'Mobile', '(234) 567-8901'),
-          _buildListItem(Icons.print, 'Fax', '(345) 678-9012'),
-          _buildListItem(Icons.email, 'Email', 'johndoe@example.com'),
-          _buildListItem(Icons.format_list_numbered, 'CRN', '123456'),
-          _buildListItem(Icons.credit_card, 'Tax ID', '123-45-6789'),
-          _buildListItem(Icons.credit_card_outlined, 'Tax ID2', '987-65-4321'),
-          _buildListItem(
-              Icons.monetization_on, 'Starting Balance', '\$1000.00'),
-          _buildListItem(Icons.trending_up, 'Turnover', '\$5000.00'),
-          _buildListItem(Icons.payment, 'Payment', '\$500.00'),
-          _buildListItem(Icons.credit_card, 'Credit', '\$200.00'),
-          _buildListItem(Icons.family_restroom, 'Family', '3'),
-        ],
+      body: ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (BuildContext context, int index) {
+          return data[index];
+        },
       ),
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Colors.blue,
